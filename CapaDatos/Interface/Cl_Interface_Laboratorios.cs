872d91fba40b2a4a9ec3df4d3ce5c761fe23ccfc
tyrone <tyronesalazar@outlook.es>
2026-01-28 11:04:03 -0500

    Nueva rama conección sql

---
+++ CapaDatos/Interface/Cl_Interface_Laboratorios.cs
@@ -0,0 +1,21 @@
+﻿using CapaDatos.SQL;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos.Interface
+{
+    public class Cl_Interface_Laboratorios
+    {
+        private Cl_ManagementSQL obj_sql = new Cl_ManagementSQL();
+
+        public DataTable GetLaboratorios()
+        {
+            string sql = "SELECT * FROM listar_laboratorios();";
+            return obj_sql.ExecuteSQLQuery(sql);
+        }
+    }
+}
--- CapaLogicaNegocio/Class1.cs
+++ CapaLogicaNegocio/Class1.cs
@@ -1,7 +1,18 @@
-﻿namespace CapaLogicaNegocio
-{
-    public class Class1
-    {
-
-    }
-}
+﻿//using CapaLogicaNegocio.LogicaNegocio;
+//using CapaLogicaNegocio.Modelos;
+//using System.Data;
+//namespace CapaLogicaNegocio
+//{
+//    internal class Class1
+//    {
+//        static void Main(string[] args)
+//        {
+//            Cl_LN_Usuarios usuario = new Cl_LN_Usuarios();
+//            DataTable dtUsuarios = usuario.GetAllUsuarios();
+//            foreach (DataRow row in dtUsuarios.Rows)
+//            {
+//                Console.WriteLine($"ID: {row["id"]}, Usuario: {row["usuario"]}");
+//            }
+//        }
+//    }
+//}
